e1cba767c4c72d7bb9122847933e15a4b72dee6d
Niyas UT <niyasut@gmail.com>
2020-01-27 09:48:54 +0530

    Project Started

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+<div class="jumbotron jumbotron-fluid bg-white">
+    <div class="container">
+        <h1 class="display-3">Welcome to Medicker</h1>
+        <p class="lead">Complete medical system</p>
+        <hr class="my-2">
+        <p>More info</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Click here</a>
+        </p>
+    </div>
+</div>
+
+@endsection
