ffcf48db4c92c6bc62931f11edeba3ca9a867024
Niyas UT <niyasut@gmail.com>
2020-02-02 16:03:07 +0530

    Home page changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,20 +43,10 @@
 </section>
 
             @yield('content')
-<<<<<<< HEAD
         
         <nav class="navbar fixed-bottom">
           <a class="navbar-brand" href="#">Copyright © 2019. Medicker. All Right Reserved</a>
         </nav>
-=======
-        </div>
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/">All rights reserved by medicker</a>
-            </li>
-</nav>
->>>>>>> 43810418a39008a5acc5fb344ce4c761131af811
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
