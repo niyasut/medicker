4984543f565d0a206882120d7c36516ab15261cf
niyasut <niyasut@gmail.com>
2020-02-04 14:13:30 +0530

    some changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,7 @@
   <p class="lead">Medical reports contain large amounts of clinical information which is not easily mined due to its unstructured format. In addition, the medical terminology is context sensitive and varies between entities. Different techniques were evaluated for future extraction. A machine learning based algorithm for automatic information extraction was developed. In this research,the medical prescription convert data from medical reports into digital format and techniques to paperless prescription on the basis of previous health data such as medicines, scanning reports,and lab reports.
 <br>
 <br>
-    In this technology tbnhhere are 5 modules are including for Doctors,Patients,Pharmacy,Labs and Scanning centres. The Doctors can view the patient previous health data such as lab reports,scanning details and previous medicines details. And they can update the patient current health data and medicines. The Patient can view there health reports prescribed by the doctor . In pharmacy they can view the current medical details of the patient. In the Lab and Scanning centres they can view the lab and scanning details prescribed by the Doctor and they can update the result</p>
+    In this technology there are 5 modules are including for Doctors,Patients,Pharmacy,Labs and Scanning centres. The Doctors can view the patient previous health data such as lab reports,scanning details and previous medicines details. And they can update the patient current health data and medicines. The Patient can view there health reports prescribed by the doctor . In pharmacy they can view the current medical details of the patient. In the Lab and Scanning centres they can view the lab and scanning details prescribed by the Doctor and they can update the result</p>
 
   
 
