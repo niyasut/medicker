10f1e7665067d48a55ac7b33759829767004d74b
niyasut <niyasut@gmail.com>
2020-03-03 15:19:36 +0530

    some changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,7 @@
 
   var i=1;
 $("#add_row").click(function(){
-$('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='user"+i+"' type='text' placeholder='Drug' class='form-control input-md'  /></td><td><input  name='pass"+i+"' type='text' placeholder='dosage'  class='form-control input-md'></td><td><input  name='ip"+i+"' type='text' placeholder='frequency'  class='form-control input-md'></td><td><input  name='country"+i+"' type='text' placeholder='days'  class='form-control input-md'></td><td><input  name='ipDisp"+i+"' type='text' placeholder='instruction'  class='form-control input-md'></td>");
+$('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='drug"+i+"' type='text' placeholder='Drug' class='form-control input-md'  /></td><td><input  name='pass"+i+"' type='text' placeholder='dosage'  class='form-control input-md'></td><td><input  name='ip"+i+"' type='text' placeholder='frequency'  class='form-control input-md'></td><td><input  name='country"+i+"' type='text' placeholder='days'  class='form-control input-md'></td><td><input  name='ipDisp"+i+"' type='text' placeholder='instruction'  class='form-control input-md'></td>");
 
 $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
 i++; 
